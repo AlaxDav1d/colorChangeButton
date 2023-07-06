53b1e9b7ba0fd8b6ee949a39aeb733811cf55cd4
AlaxDav1d <alaxdavid13@gmai.com>
2023-07-06 14:04:09 -0300

    projeto com botoes e roda de cores para mudar o cor do background

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+var el = document.querySelector("body")
+
+function azul()
+    {
+        var el_1 = document.querySelector('#azul')
+        el.style.backgroundColor = 'rgb(69,69,244)';
+     
+    }
+function vermelho()
+    {   
+      
+        el.style.backgroundColor = 'rgb(203,11,11)'
+    
+    }
+function verde()
+    {
+        var verde = document.querySelector('verde')
+        el.style.backgroundColor = 'rgb(9,203,9)';
+         
+    }
+function color()
+    {
+        var cor = document.querySelector('#cor')
+        el.style.backgroundColor = cor.value
+    }
+function check()
+    {
+        var check = document.querySelector('#check')
+        el.style.backgroundColor = 'green'
+    }
+function check_1()
+    {
+        el.style.backgroundColor = 'white'
+    }
+      
